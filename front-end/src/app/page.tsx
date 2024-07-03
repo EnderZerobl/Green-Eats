755720e8796cbb23f4cc5e3f2dc42a9fa0b647f9
arthur batista <arthurbat13.as@gmail.com>
2024-07-03 15:15:53 -0300

    feat: adiciona o header da seção dos produtos

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/hero/Hero";
+import ProductSection from "@/components/productSection/ProductSection";
 import { createClient } from "@/prismicio";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ProductSection />
     </main>
   );
 }
--- front-end/src/components/header/Header.tsx
+++ front-end/src/components/header/Header.tsx
@@ -1,23 +1,49 @@
+"use client"
+
 import './Header.css';
 import Image from 'next/image';
 import Link from 'next/link';
-import searchIcon from "../../../public/search.svg"
-import marketIcon from "../../../public/market.svg"
-import profileIcon from "../../../public/profile.svg"
-
+import { useEffect } from 'react';
+import searchIcon from "../../../public/search.svg";
+import marketIcon from "../../../public/market.svg";
+import profileIcon from "../../../public/profile.svg";
 
 export default function Header() {
+
+    useEffect(() => {
+        const searchIconContainer = document.querySelector('.searchIconContainer');
+        const searchContainer = document.querySelector('.searchContainer');
+        const searchInput = document.querySelector('.searchInput');
+
+        if (searchIconContainer && searchContainer) {
+            const handleMouseOver = () => {
+                (searchInput as HTMLElement).style.border = '2px solid var(--black-green)';
+            };
+            const handleMouseOut = () => {
+                (searchInput as HTMLElement).style.border = '';
+            };
+
+            searchIconContainer.addEventListener('mouseover', handleMouseOver);
+            searchIconContainer.addEventListener('mouseout', handleMouseOut);
+
+            return () => {
+                searchIconContainer.removeEventListener('mouseover', handleMouseOver);
+                searchIconContainer.removeEventListener('mouseout', handleMouseOut);
+            };
+        }
+    }, []);
+
     return (
         <>
-        <header>
-            <div className="topContentContainer">
+            <header>
+                <div className="topContentContainer">
                     <div className="logo">
                         <h1>Green Eats</h1>
                     </div>
                     <div className="searchContainer">
-                        <input type="text" name="" id="" placeholder="Nome do produto"/>
+                        <input type="text" name="" id="" placeholder="Nome do produto" className="searchInput"/>
                         <button className="searchIconContainer">
-                            <Image src={searchIcon} width={0} height={0} alt="Ícone de pesquisar"/>
+                            <Image src={searchIcon} width={0} height={0} alt="Ícone de pesquisar" />
                         </button>
                     </div>
 
@@ -30,40 +56,36 @@ export default function Header() {
                         </div>
                     </div>
                 </div>
-
-        </header>
-        <nav>
-            <div className="navContainer">
-                <div className="navTextContainer">
-                    <Link href="/">
-                        <p>Promoções</p>
-                    </Link>
+            </header>
+            <nav>
+                <div className="navContainer">
+                    <div className="navTextContainer">
+                        <Link href="/">
+                            <p>Promoções</p>
+                        </Link>
+                    </div>
+                    <div className="navTextContainer">
+                        <Link href="/">
+                            <p>Green Horta</p>
+                        </Link>
+                    </div>
+                    <div className="navTextContainer bigger">
+                        <Link href="/">
+                            <p>Green Mercearia</p>
+                        </Link>
+                    </div>
+                    <div className="navTextContainer bigger">
+                        <Link href="/">
+                            <p>Bebidas e Laticínios</p>
+                        </Link>
+                    </div>
+                    <div className="navTextContainer">
+                        <Link href="/">
+                            <p>Ovos e Carnes</p>
+                        </Link>
+                    </div>
                 </div>
-                <div className="navTextContainer">
-                    <Link href="/">
-                        <p>Green Horta</p>
-                    </Link>
-                </div>
-                <div className="navTextContainer bigger">
-                    <Link href="/">
-                        <p>Green Mercearia</p>
-                    </Link>
-                    
-                </div>
-                <div className="navTextContainer bigger">
-                    <Link href="/">
-                        <p>Bebidas e Laticínios</p>
-                    </Link>
-                </div>
-                <div className="navTextContainer">
-                    <Link href="/">
-                        <p>Ovos e Carnes</p>
-                    </Link>
-                </div>
-
-            </div>
-            
-        </nav>
+            </nav>
         </>
-    )
+    );
 };
