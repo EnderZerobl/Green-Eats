2b97e990ff6ac81f59b6b236e511210ca21d3fe1
Bruno-Emanoel <brunoemanoel.l.x@gmail.com>
2024-07-08 23:33:26 -0300

    feat: fixing the catalogue and the hero

--- front-end/src/components/hero/Hero.tsx
+++ front-end/src/components/hero/Hero.tsx
@@ -59,7 +59,7 @@ export default function Hero({ page }: {
 }) {
     // Carousel settings
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
